748eceb58cee2c4cba57132d396a8021c2f73f9d
Mohammad-Saad <saad.ash29@gmail.com>
2015-04-09 06:56:37 -0700

    custom pickerview

--- coustom picker view/ViewController.swift
+++ coustom picker view/ViewController.swift
@@ -8,7 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    
+    let modarray = ["abc","def","fdsdsa","fdasfsda","fsadfdsa"]
+    var var2=["abc","d","fd","f","dsa"]
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            var rows = var2.count
+            if component == 0
+            {
+                rows = modarray.count
+            }
+            return rows;
+        }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        var componentStr = var2[row]
+        if component == 0
+        {
+            componentStr = modarray[row]
+        }
+        return componentStr;
+        }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
